d25ffbfacc84a989e46656cba494ce38663d9fab
xgdgsc <xgdgsc@users.noreply.github.com>
2025-08-22 22:55:07 +0800

    add supervise

--- src/JobSchedulers.jl
+++ src/JobSchedulers.jl
@@ -70,6 +70,9 @@ export queue_progress
 include("macro.jl")
 export @submit
 
+include("supervisor.jl")
+export supervise
+
 function __init__()
     # Fixing precompilation hangs due to open tasks or IO
     # https://docs.julialang.org/en/v1/devdocs/precompile_hang/
