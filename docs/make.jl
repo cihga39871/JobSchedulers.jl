a8a083aea8c113e39577979d3104727fe14808b5
Jiacheng Chuan <jiacheng_chuan@outlook.com>
2025-10-10 07:36:19 -0300

    docs: update logo and build script

--- docs/make.jl
+++ docs/make.jl
@@ -2,7 +2,8 @@ using Documenter
 using JobSchedulers
 
 makedocs(
-    sitename="JobSchedulers.jl",
+    sitename = "JobSchedulers.jl",
+    authors = "Dr. Jiacheng Chuan, and contributors.",
     pages = [
         "Home" => "index.md",
         "Manual" => "manual.md",
@@ -12,10 +13,15 @@ makedocs(
         "API" => "API.md",
         "Change Log" => "changelog.md"
     ],
-    format = Documenter.HTML(sidebar_sitename=false)
+    format = Documenter.HTML(
+        sidebar_sitename=false,
+        assets = ["assets/favicon.ico"]
+    )
 )
 
-deploydocs(
-    repo = "github.com/cihga39871/JobSchedulers.jl.git",
-    devbranch = "main"
-)
+if haskey(ENV, "GITHUB_TOKEN")
+    deploydocs(
+        repo = "github.com/cihga39871/JobSchedulers.jl.git",
+        devbranch = "main"
+    )
+end
